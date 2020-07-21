792de6b6b1cd5715a8f1d6a41e2102fd9bd1e1d7
Victor Azevedo <victorazevedomagalhaes@gmail.com>
2020-07-21 17:02:12 -0300

    finalização do calculo de datas

--- Datas/datas.js
+++ Datas/datas.js
@@ -1,3 +1,16 @@
-var nascimento = document.getElementById("anonascimento").value
-var atual = document.getElementById("anoatual").value
 
+var  nascimento = Date.parse(document.getElementById("anonascimento").value)
+var atual = Date.parse(document.getElementById("anoatual").value)
+var anouser;
+
+function calcular (){
+    anouser = atual - nascimento
+    //talvez tenha um jeito mais "simples" de converter os números
+    var dataanouser = new Date(anouser)
+    var idadeEmSeg = dataanouser / 1000
+    var idadeEmMin = dataanouser / 60000
+    var idadeEmHoras = dataanouser / 3600000
+    var idadeEmDias = dataanouser / 86400000
+    var idadeEmAnos = Math.floor(dataanouser / 31536000000)
+    alert(`Você já viveu ${idadeEmSeg} segundos, ${idadeEmMin} minutos, ${idadeEmHoras} horas, ${idadeEmDias} dias e ${idadeEmAnos} anos `)
+}
